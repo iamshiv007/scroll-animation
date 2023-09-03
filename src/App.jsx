86c2f7cdd7793b6f708326e1599b8854004d3d44
iamshiv007 <iamshiv20032003@gmail.com>
2023-09-03 21:16:47 +0530

    feat: App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
 import "./App.css";
 
 function App() {
-  return <>Project with react and vite</>;
+  return (
+    <>
+      <div className='app'>
+        <header>This is The header</header>
+        <main>
+          <div className='child-one'>Child One</div>
+          <div className='child-two'>Child Two</div>
+        </main>
+        <footer>This is The Footer</footer>
+      </div>
+    </>
+  );
 }
 
 export default App;
